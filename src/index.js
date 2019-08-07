cf844283242a704ee74dd821a9c1b98a38432651
Erik Antillanca Pichún <eantillanca@inf.uct.cl>
2019-08-07 16:18:55 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+const element = <h1>Hello, Platzi Badges eantillanca!</h1>;
 
 const container = document.getElementById('app');
 
